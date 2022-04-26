bdd8b36ca404e24a7fe1396ef515cda32ce7690d
Tristan Monteiro <71944372+Git-TM@users.noreply.github.com>
2022-04-26 15:10:59 +0200

    Added a navbar and some style for the sign up and sign in forms

---
+++ test/mailers/password_forgotten_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PasswordForgottenMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/password_forgotten_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/password_forgotten_mailer
+class PasswordForgottenMailerPreview < ActionMailer::Preview
+
+end
